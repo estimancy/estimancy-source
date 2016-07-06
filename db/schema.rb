ecbc3457d0c2b332d04e74f7dfa45346b88a1da5
nicolasrenard <renard760@gmail.com>
2016-07-06 14:22:42 +0200

    Bug

--- db/schema.rb
+++ db/schema.rb
@@ -865,6 +865,41 @@ ActiveRecord::Schema.define(:version => 20160526133627) do
   add_index "master_settings", ["reference_id"], :name => "index_master_settings_on_parent_id"
   add_index "master_settings", ["uuid"], :name => "index_master_settings_on_uuid", :unique => true
 
+  create_table "module_project_ratio_elements", :force => true do |t|
+    t.integer  "pbs_project_element_id"
+    t.integer  "module_project_id"
+    t.integer  "wbs_activity_ratio_id"
+    t.integer  "wbs_activity_ratio_element_id"
+    t.integer  "wbs_activity_element_id"
+    t.boolean  "multiple_references"
+    t.string   "name"
+    t.text     "description"
+    t.float    "ratio_value"
+    t.float    "theoretical_effort_probable"
+    t.float    "theoretical_cost_probable"
+    t.float    "retained_effort_probable"
+    t.float    "retained_cost_probable"
+    t.text     "comments"
+    t.float    "theoretical_effort_low"
+    t.float    "theoretical_effort_high"
+    t.float    "theoretical_effort_most_likely"
+    t.float    "theoretical_cost_low"
+    t.float    "theoretical_cost_high"
+    t.float    "theoretical_cost_most_likely"
+    t.float    "retained_effort_low"
+    t.float    "retained_effort_high"
+    t.float    "retained_effort_most_likely"
+    t.float    "retained_cost_low"
+    t.float    "retained_cost_high"
+    t.float    "retained_cost_most_likely"
+    t.integer  "copy_id"
+    t.float    "position"
+    t.boolean  "flagged"
+    t.boolean  "selected"
+    t.datetime "created_at",                     :null => false
+    t.datetime "updated_at",                     :null => false
+  end
+
   create_table "module_projects", :force => true do |t|
     t.integer  "pemodule_id"
     t.integer  "project_id"
