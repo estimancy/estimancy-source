97684b785802ed7af422eeae57914689ac6721ae
nicolasrenard <renard760@gmail.com>
2016-12-27 15:53:43 +0100

    Bugs GUW

--- vendor/gems/guw/app/controllers/guw/guw_attribute_types_controller.rb
+++ vendor/gems/guw/app/controllers/guw/guw_attribute_types_controller.rb
@@ -3,11 +3,11 @@ class Guw::GuwAttributeTypesController < ApplicationController
   def edit
     @guw_model = Guw::GuwModel.where(params[:guw_model_id]).first
     @guw_type = Guw::GuwType.where(params[:guw_type_id]).first
-    @guw_attribute_type = Guw::GuwAttributeType.where(params[:id]).first
+    @guw_attribute_type = Guw::GuwAttributeType.where(id: params[:id]).first
   end
 
   def update
-    @guw_attribute_type = Guw::GuwAttributeType.where(params[:id]).first
+    @guw_attribute_type = Guw::GuwAttributeType.where(id: params[:id]).first
     @guw_attribute_type.default_value = params[:guw_attribute_type][:default_value]
     @guw_attribute_type.save
 
