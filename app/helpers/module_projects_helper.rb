4a1e9f141b0716e0573a2623c57a252339fc039b
gsaly <gueyesaly@yahoo.fr>
2017-01-13 17:38:35 +0100

    Vignettes : unité des vignettes des attributs "Effort" #1176  et Mise au norme des valeurs à zero ou à null

--- app/helpers/module_projects_helper.rb
+++ app/helpers/module_projects_helper.rb
@@ -25,9 +25,9 @@ module ModuleProjectsHelper
   # results: estimation result for (low, most_likely, high)
   # estimation_value : the estimation_value object
   def probable_value(results, estimation_value)
-    minimum = 0.0
-    most_likely = 0.0
-    maximum = 0.0
+    minimum = nil #0.0
+    most_likely = nil #0.0
+    maximum = nil #0.0
     attribute_alias = estimation_value.pe_attribute.alias.to_sym
     #puts "RESULT_FOR_PROBABLE = #{results}"
     min_estimation_value = results[:low]["#{estimation_value.pe_attribute.alias}_#{estimation_value.module_project_id.to_s}".to_sym]
@@ -62,7 +62,7 @@ module ModuleProjectsHelper
   end
 
   #Function that compute the probable value according to the number of nut null value
-  def compute_probable_value(minimum, most_likely, maximum, estimation_value=nil)
+  def compute_probable_value_avec_zero(minimum, most_likely, maximum, estimation_value=nil)
     # Get the number of not null value
     input_data = {:min => minimum, :ml => most_likely, :max => maximum}
     not_integer_or_float = Array.new
@@ -119,6 +119,66 @@ module ModuleProjectsHelper
       ###if estimation_value.pe_attribute.attr_type.eql?("integer") && !computed_probable_value.nan?
       ###if computed_probable_value.is_a?(Integer) || (computed_probable_value.is_a?(Float) && !computed_probable_value.nan?)
       unless computed_probable_value.is_a?(Float) && computed_probable_value.nan?
+        computed_probable_value = computed_probable_value
+      end
+    end
+
+    {:value => computed_probable_value, :is_consistent => consistency}
+  end
+
+  #Function that compute the probable value according to the number of nut null value
+  def compute_probable_value(minimum, most_likely, maximum, estimation_value=nil)
+    # Get the number of not null value
+    input_data = {:min => minimum, :ml => most_likely, :max => maximum}
+    not_integer_or_float = Array.new
+    sum_of_not_null = nil#0.0
+    computed_probable_value = nil #0.0
+
+    # leaf element consistency will be compute at the same time
+    consistency = true
+
+    number_of_not_null = nil #0.0
+    value_to_compute_for_probable = []
+    input_data.each do |key, value|
+      ###if value.is_a?(Integer) || value.is_a?(Float) || value.is_a?(BigDecimal) || value.is_a?(Bignum)
+      if value.is_a?(Integer) || value.is_a?(Float) || value.class.superclass == Integer || value.class.superclass == Numeric
+        begin
+          if key.to_s.eql?("ml")
+            unless value.nil?
+              number_of_not_null = number_of_not_null.to_i+4
+              value_to_compute_for_probable << 4*value.to_f
+            end
+          else
+            unless value.nil?
+              number_of_not_null = number_of_not_null.to_i+1
+              value_to_compute_for_probable << value.to_f
+            end
+          end
+        rescue
+          not_integer_or_float << nil
+        end
+      else
+        not_integer_or_float << "#{key.to_s}"
+      end
+    end
+
+    # If there is no null value, probable value is calculated normally as follow
+    if value_to_compute_for_probable.empty?
+      computed_probable_value = nil
+    else
+      if value_to_compute_for_probable.length == 3
+        computed_probable_value = value_to_compute_for_probable.sum / 6
+      else
+        # Calculate the probable value according to the number of not null value (sum is divide by the number od not null values)
+      computed_probable_value = number_of_not_null.nil? ? nil : (value_to_compute_for_probable.sum / number_of_not_null)
+      end
+    end
+
+    unless estimation_value.nil?
+      #computed_probable_value according to the attribute type
+      ###if estimation_value.pe_attribute.attr_type.eql?("integer") && !computed_probable_value.nan?
+      ###if computed_probable_value.is_a?(Integer) || (computed_probable_value.is_a?(Float) && !computed_probable_value.nan?)
+      unless computed_probable_value.nil? || (computed_probable_value.is_a?(Float) && computed_probable_value.nan?)
         computed_probable_value = computed_probable_value
       end
     end
