9b51c37530150fc9188fa1bbebead6f0c6494102
nicolasrenard <renard760@gmail.com>
2017-02-08 16:22:25 +0100

    Export

--- vendor/gems/skb/app/controllers/skb/skb_models_controller.rb
+++ vendor/gems/skb/app/controllers/skb/skb_models_controller.rb
@@ -100,6 +100,15 @@ class Skb::SkbModelsController < ApplicationController
     default_attributs = ["Nom", "Description", "Date", "Données", "Traitements"]
 
     if !skb_model_datas.nil? && !skb_model_datas.empty?
+
+      worksheet.add_cell(0, 0, "ID").change_horizontal_alignment('center')
+      worksheet.add_cell(0, 1, "Nom").change_horizontal_alignment('center')
+      worksheet.add_cell(0, 2, "Date").change_horizontal_alignment('center')
+      worksheet.add_cell(0, 3, "X").change_horizontal_alignment('center')
+      worksheet.add_cell(0, 4, "Y").change_horizontal_alignment('center')
+      worksheet.add_cell(0, 5, "F1").change_horizontal_alignment('center')
+      worksheet.add_cell(0, 6, "F2").change_horizontal_alignment('center')
+
       skb_model_datas.each_with_index do |skb_data, index|
         worksheet.add_cell(index + 1, 0, skb_data.name).change_horizontal_alignment('center')
         worksheet.add_cell(index + 1, 1, skb_data.description).change_horizontal_alignment('center')
@@ -107,7 +116,7 @@ class Skb::SkbModelsController < ApplicationController
         worksheet.add_cell(index + 1, 3, skb_data.data).change_horizontal_alignment('center')
         worksheet.add_cell(index + 1, 4, skb_data.processing).change_horizontal_alignment('center')
         skb_data.custom_attributes.each_with_index  do |(custom_attr_k, custom_attr_v),index_2|
-          worksheet.add_cell(index + 1, index_2 + 4, custom_attr_v).change_horizontal_alignment('center')
+          worksheet.add_cell(index + 1, index_2 + 5, custom_attr_v).change_horizontal_alignment('center')
           if index_2 + 3 > 2
             default_attributs.include?(custom_attr_k.to_s) ? default_attributs : default_attributs  << custom_attr_k.to_s
           end
