be29af14e9b1dff38cb9c06e0f1fe7cc0b9be76b
gsaly <gueyesaly@yahoo.fr>
2017-03-03 09:34:39 +0100

    WBS-Activity - Gestion Compatibilité anciens module_projects avec la gestion des 4 entrées + MAJ copie de la Wbs- activité #1181

--- app/models/module_project.rb
+++ app/models/module_project.rb
@@ -215,19 +215,31 @@ class ModuleProject < ActiveRecord::Base
   # Get the module_project ratio-elements for WBS-ACTIVITY module
   def get_module_project_ratio_elements(wbs_activity_ratio, pbs_project_element, sort_result=true)
     # Module_project Ratio elements
-    mp_ratio_elements = self.module_project_ratio_elements.where(wbs_activity_ratio_id: wbs_activity_ratio.id, pbs_project_element_id: pbs_project_element.id)
+    #mp_ratio_elements = self.module_project_ratio_elements.where(wbs_activity_ratio_id: wbs_activity_ratio.id, pbs_project_element_id: pbs_project_element.id)
 
     ####if  mp_ratio_elements.nil? || mp_ratio_elements.all.empty?
     #create module_project ratio elements
     wbs_activity_ratio.wbs_activity_ratio_elements.each do |ratio_element|
-      mp_ratio_elt = ModuleProjectRatioElement.where(pbs_project_element_id: pbs_project_element.id, module_project_id: self.id, wbs_activity_ratio_id: wbs_activity_ratio.id, wbs_activity_ratio_element_id: ratio_element.id).first
+      #mp_ratio_elt = ModuleProjectRatioElement.where(pbs_project_element_id: pbs_project_element.id, module_project_id: self.id, wbs_activity_ratio_id: wbs_activity_ratio.id, wbs_activity_ratio_element_id: ratio_element.id).first
+      mp_ratio_elt = ModuleProjectRatioElement.where(pbs_project_element_id: pbs_project_element.id, module_project_id: self.id,
+                                                     wbs_activity_ratio_id: wbs_activity_ratio.id, wbs_activity_ratio_element_id: ratio_element.id,
+                                                     wbs_activity_element_id: ratio_element.wbs_activity_element_id).first
 
       if mp_ratio_elt.nil?
-        mp_ratio_elt = ModuleProjectRatioElement.create(pbs_project_element_id: pbs_project_element.id, module_project_id: self.id, wbs_activity_ratio_id: wbs_activity_ratio.id,
+        mp_ratio_elt = ModuleProjectRatioElement.where(pbs_project_element_id: pbs_project_element.id, module_project_id: self.id,
+                                                       wbs_activity_ratio_id: wbs_activity_ratio.id, wbs_activity_ratio_element_id: ratio_element.id).first
+
+        if !mp_ratio_elt.nil?
+          mp_ratio_elt.update_attributes(pbs_project_element_id: pbs_project_element.id, module_project_id: self.id, wbs_activity_ratio_id: wbs_activity_ratio.id,
+                                         is_optional: ratio_element.is_optional, wbs_activity_ratio_element_id: ratio_element.id, multiple_references: ratio_element.multiple_references,
+                                         wbs_activity_element_id: ratio_element.wbs_activity_element_id, name: ratio_element.wbs_activity_element.name, description: ratio_element.wbs_activity_element.description,
+                                         ratio_value: ratio_element.ratio_value, position: ratio_element.wbs_activity_element.position, selected: true)
+        else
+          mp_ratio_elt = ModuleProjectRatioElement.create(pbs_project_element_id: pbs_project_element.id, module_project_id: self.id, wbs_activity_ratio_id: wbs_activity_ratio.id,
                                                         is_optional: ratio_element.is_optional, wbs_activity_ratio_element_id: ratio_element.id, multiple_references: ratio_element.multiple_references,
                                                         wbs_activity_element_id: ratio_element.wbs_activity_element_id, name: ratio_element.wbs_activity_element.name, description: ratio_element.wbs_activity_element.description,
                                                         ratio_value: ratio_element.ratio_value, position: ratio_element.wbs_activity_element.position, selected: true)
-
+        end
       end
 
       current_ratio_mp_ratio_elements = self.module_project_ratio_elements.where(wbs_activity_ratio_id: wbs_activity_ratio.id)
