9ae7744cbe8d96583d9479a5beb238cc090e08c3
gsaly <gueyesaly@yahoo.fr>
2017-04-28 16:44:10 +0200

    Copie d'organisation ne s'effectue pas bien #1224

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1248,7 +1248,7 @@ class OrganizationsController < ApplicationController
         format.html { redirect_to organizationals_params_path and return }
         #format.js { render :js => "window.location.replace('/organizationals_params');"}
         ##format.js { render :js => "alert('Fin de copie: la nouvelle organisation a été créée avec succès'); window.location.replace('/organizationals_params');"}
-        format.js { render :js => "alert('Fin de copie: la nouvelle organisation a été créée avec succès. Veuiller recharger la page pour voir apparaître votre nouvelle organisation.'); "}
+        format.js { render :js => 'alert("Fin de copie: la nouvelle organisation a été créée avec succès.\n Veuiller recharger la page pour voir apparaître votre nouvelle organisation."); '}
       end
 
     rescue
