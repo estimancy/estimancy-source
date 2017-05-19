c8b60d526125a18631fe3c39ab0eddcc4e7b9be7
nicolasrenard <renard760@gmail.com>
2017-05-19 16:12:06 +0200

    GUW : Déplacement des CPLX

--- vendor/gems/skb/app/controllers/skb/skb_models_controller.rb
+++ vendor/gems/skb/app/controllers/skb/skb_models_controller.rb
@@ -246,7 +246,11 @@ class Skb::SkbModelsController < ApplicationController
                             skb_model_id: @skb_model.id)
       end
     end
-    redirect_to skb.edit_skb_model_path(@skb_model.id, organization_id: @organization.id)
+    if @skb_model.nil?
+      redirect_to :back
+    else
+      redirect_to skb.edit_skb_model_path(@skb_model.id, organization_id: @organization.id)
+    end
   end
 
   def save_filters
