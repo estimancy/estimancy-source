a3c84a6123a2275fda5ba5fd41ef11c49327c726
nicolasrenard <renard760@gmail.com>
2017-06-28 11:48:50 +0200

    ML Data

--- vendor/gems/guw/app/controllers/guw/guw_unit_of_works_controller.rb
+++ vendor/gems/guw/app/controllers/guw/guw_unit_of_works_controller.rb
@@ -1580,7 +1580,9 @@ class Guw::GuwUnitOfWorksController < ApplicationController
 
         tab = worksheet.extract_data
         tab.each_with_index do |row, index|
-          txt << row[5].to_s
+          unless row.nil?
+            txt << row[5].to_s
+          end
         end
 
         @guw_type = Guw::GuwType.where(guw_model_id: @guw_model.id).last
