fdaa121f34786607be28409d834c083d84a55282
gsaly <gueyesaly@yahoo.fr>
2017-08-28 12:12:35 +0200

    Delete migrations

--- db/migrate/20170814083159_add_allow_modif_effort_to_wbs_activity_elements.rb
+++
@@ -1,5 +0,0 @@
-class AddAllowModifEffortToWbsActivityElements < ActiveRecord::Migration
-  def change
-    add_column :wbs_activity_elements, :allow_modif_effort, :boolean
-  end
-end
--- db/migrate/20170814083216_add_allow_modif_cost_to_wbs_activity_elements.rb
+++
@@ -1,5 +0,0 @@
-class AddAllowModifCostToWbsActivityElements < ActiveRecord::Migration
-  def change
-    add_column :wbs_activity_elements, :allow_modif_cost, :boolean
-  end
-end
