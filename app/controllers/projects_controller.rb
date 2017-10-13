c906be1bb7fcac4fcf8d0a5528f4f5aed1baacf6
gsaly <gueyesaly@yahoo.fr>
2017-10-13 18:16:56 +0200

    Créer/Modifier la vue des estimations pour mieux gérer la recherche et le tri dans la liste des estimations, notamment pour les valeurs numériques refs: #1570

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2412,7 +2412,7 @@ public
   def sort
     @organization = @current_organization
     # @projects = @organization.organization_estimations
-    @projects = @organization.projects.where('is_model IS NOT TRUE')
+    @projects = @organization.projects#.where('is_model IS NOT TRUE')
 
     k = params[:f]
     s = params[:s]
@@ -2421,53 +2421,56 @@ public
     @sort_order = s
 
     ### Les tris par défaut ( date descendant et title ascendant)
-    if k != "start_date"
-      @projects = @projects.unscoped.order("start_date desc")
-    end
+    # if k != "start_date"
+    #   @projects = @projects.unscoped.order("start_date desc")
+    # end
 
     case k
       when "title"
-        @projects = @projects.unscoped.order("title #{s}").all
+        @projects = @projects.unscoped.order("title #{s}")#.all
       when "description"
-        @projects = @projects.unscoped.order("description #{s}").all
+        @projects = @projects.unscoped.order("description #{s}")#.all
       when "project_area"
         @projects = Project.unscoped
                         .joins("LEFT JOIN project_areas ON projects.project_area_id = project_areas.id")
                         .where(organization_id: @organization.id)
-                        .order("project_areas.name #{s}").all
+                        .order("project_areas.name #{s}")#.all
       when "project_category"
         @projects = Project.unscoped
                         .joins("LEFT JOIN project_categories ON projects.project_category_id = project_categories.id")
                         .where(organization_id: @organization.id)
-                        .order("project_categories.name #{s}").all
+                        .order("project_categories.name #{s}")#.all
       when "platform_category"
         @projects = Project.unscoped
                         .joins("LEFT JOIN platform_categories ON projects.platform_category_id = platform_categories.id")
                         .where(organization_id: @organization.id)
-                        .order("platform_categories.name #{s}").all
+                        .order("platform_categories.name #{s}")#.all
       when "acquisition_category"
         @projects = Project.unscoped
                         .joins("LEFT JOIN acquisition_categories ON projects.acquisition_category_id = acquisition_categories.id")
                         .where(organization_id: @organization.id)
-                        .order("acquisition_categories.name #{s}").all
+                        .order("acquisition_categories.name #{s}")#.all
       when "status_name"
         @projects = Project.unscoped
                         .joins("LEFT JOIN estimation_statuses ON projects.estimation_status_id = estimation_statuses.id")
                         .where(organization_id: @organization.id)
-                        .order("estimation_statuses.name #{s}").all
+                        .order("estimation_statuses.name #{s}")#.all
       when "status_name"
         @projects = Project.unscoped
                         .joins("LEFT JOIN estimation_statuses ON projects.estimation_status_id = estimation_statuses.id")
                         .where(organization_id: @organization.id)
-                        .order("estimation_statuses.name #{s}").all
+                        .order("estimation_statuses.name #{s}")#.all
       when "start_date"
-        @projects = @projects.unscoped.order("start_date #{s}").all
+        @projects = @projects.unscoped.order("start_date #{s}")#.all
       when "updated_at"
-        @projects = @projects.unscoped.order("updated_at #{s}").all
+        @projects = @projects.unscoped.order("updated_at #{s}")#.all
       when "created_at"
-        @projects = @projects.unscoped.order("created_at #{s}").all
+        @projects = @projects.unscoped.order("created_at #{s}")#.all
       else
     end
+
+
+    @projects = @projects.where(:is_model => [nil, false]).all
 
     res = []
     @projects.each do |p|
