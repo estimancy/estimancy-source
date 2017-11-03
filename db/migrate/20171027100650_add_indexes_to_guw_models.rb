dc5953cca2a87af04ff93509101834bb65fc2d97
gsaly <gueyesaly@yahoo.fr>
2017-11-03 15:42:10 +0100

    Module de Taille (UO générique) Exécution : Amélioration de la performance refs: #1738 et #1771

--- db/migrate/20171027100650_add_indexes_to_guw_models.rb
+++ db/migrate/20171027100650_add_indexes_to_guw_models.rb
@@ -1,47 +1,47 @@
 class AddIndexesToGuwModels < ActiveRecord::Migration
   def up
-    # #Guw::GuwUnitOfWorkGroup
-    # add_index :guw_guw_unit_of_work_groups, [:module_project_id, :pbs_project_element_id, :name], name: :module_project_guw_groups unless index_exists?(:guw_guw_unit_of_work_groups, [:module_project_id, :pbs_project_element_id, :name], name: :module_project_guw_groups)
-    #
-    # #Guw::GuwType
-    # add_index :guw_guw_types, [:guw_model_id, :name], name: :guw_model_guw_types unless index_exists?(:guw_guw_types, [:guw_model_id, :name], name: :guw_model_guw_types)
-    # add_index :guw_guw_types, [:guw_model_id, :is_default], name: :guw_model_default_guw_types unless index_exists?(:guw_guw_types, [:guw_model_id, :is_default], name: :guw_model_default_guw_types)
-    #
-    # #Guw::GuwUnitOfWork
+    #Guw::GuwUnitOfWorkGroup
+    add_index :guw_guw_unit_of_work_groups, [:module_project_id, :pbs_project_element_id, :name], name: :module_project_guw_groups unless index_exists?(:guw_guw_unit_of_work_groups, [:module_project_id, :pbs_project_element_id, :name], name: :module_project_guw_groups)
+
+    #Guw::GuwType
+    add_index :guw_guw_types, [:guw_model_id, :name], name: :guw_model_guw_types unless index_exists?(:guw_guw_types, [:guw_model_id, :name], name: :guw_model_guw_types)
+    add_index :guw_guw_types, [:guw_model_id, :is_default], name: :guw_model_default_guw_types unless index_exists?(:guw_guw_types, [:guw_model_id, :is_default], name: :guw_model_default_guw_types)
+
+    #Guw::GuwUnitOfWork
     add_index :guw_guw_unit_of_works, [:guw_model_id, :module_project_id, :pbs_project_element_id, :guw_unit_of_work_group_id, :guw_type_id, :selected], name: :module_project_guw_unit_of_works unless index_exists?(:guw_guw_unit_of_works, [:guw_model_id, :module_project_id, :pbs_project_element_id, :guw_unit_of_work_group_id, :guw_type_id, :selected], name: :module_project_guw_unit_of_works)
-    #
-    # #Guw::GuwComplexity
-    # add_index :guw_guw_complexities, [:guw_type_id, :name], name: :guw_type_complexities unless index_exists?(:guw_guw_complexities, [:guw_type_id, :name], name: :guw_type_complexities)
-    #
-    # #Guw::GuwCoefficient
-    # add_index :guw_guw_coefficients, [:guw_model_id, :name], name: :guw_model_guw_coefficients unless index_exists?(:guw_guw_coefficients, [:guw_model_id, :name], name: :guw_model_guw_coefficients)
-    #
-    # #Guw::GuwOutput
-    # add_index :guw_guw_outputs, [:guw_model_id, :name], name: :guw_model_guw_outputs unless index_exists?(:guw_guw_outputs, [:guw_model_id, :name], name: :guw_model_guw_outputs)
-    #
-    # #Guw::GuwUnitOfWorkAttribute index important car plusieurs requetes
-    # add_index :guw_guw_unit_of_work_attributes, [:guw_type_id, :guw_attribute_id, :guw_unit_of_work_id], name: :guw_unit_of_work_attributes unless index_exists?(:guw_guw_unit_of_work_attributes, [:guw_type_id, :guw_attribute_id, :guw_unit_of_work_id], name: :guw_unit_of_work_attributes)
-    #
-    # #Guw::GuwCoefficientElementUnitOfWork
-    # add_index :guw_guw_coefficient_element_unit_of_works, [:guw_unit_of_work_id, :guw_coefficient_id, :guw_coefficient_element_id], name: :guw_unit_of_work_guw_coefficient_elements unless index_exists?(:guw_guw_coefficient_element_unit_of_works, [:guw_unit_of_work_id, :guw_coefficient_id, :guw_coefficient_element_id], name: :guw_unit_of_work_guw_coefficient_elements)
-    #
-    # #Guw::GuwOutputComplexity
-    # add_index :guw_guw_output_complexities, [:guw_complexity_id, :guw_output_id], name: :guw_output_complexities unless index_exists?(:guw_guw_output_complexities, [:guw_complexity_id, :guw_output_id], name: :guw_output_complexities)
-    #
-    # #Guw::GuwOutputComplexityInitialization
-    # add_index :guw_guw_output_complexity_initializations, [:guw_complexity_id, :guw_output_id], name: :guw_output_complexity_initializations unless index_exists?(:guw_guw_output_complexity_initializations, [ :guw_complexity_id, :guw_output_id], name: :guw_output_complexity_initializations)
-    #
-    # #Guw::GuwScaleModuleAttribute
-    # add_index :guw_guw_scale_module_attributes, [:guw_model_id, :type_attribute], name: :guw_scale_module_attributes unless index_exists?(:guw_guw_scale_module_attributes, [:guw_model_id, :type_attribute], name: :guw_scale_module_attributes)
-    #
-    # #Guw::GuwAttributeComplexity
+
+    #Guw::GuwComplexity
+    add_index :guw_guw_complexities, [:guw_type_id, :name], name: :guw_type_complexities unless index_exists?(:guw_guw_complexities, [:guw_type_id, :name], name: :guw_type_complexities)
+
+    #Guw::GuwCoefficient
+    add_index :guw_guw_coefficients, [:guw_model_id, :name], name: :guw_model_guw_coefficients unless index_exists?(:guw_guw_coefficients, [:guw_model_id, :name], name: :guw_model_guw_coefficients)
+
+    #Guw::GuwOutput
+    add_index :guw_guw_outputs, [:guw_model_id, :name], name: :guw_model_guw_outputs unless index_exists?(:guw_guw_outputs, [:guw_model_id, :name], name: :guw_model_guw_outputs)
+
+    #Guw::GuwUnitOfWorkAttribute index important car plusieurs requetes
+    add_index :guw_guw_unit_of_work_attributes, [:guw_type_id, :guw_attribute_id, :guw_unit_of_work_id], name: :guw_unit_of_work_attributes unless index_exists?(:guw_guw_unit_of_work_attributes, [:guw_type_id, :guw_attribute_id, :guw_unit_of_work_id], name: :guw_unit_of_work_attributes)
+
+    #Guw::GuwCoefficientElementUnitOfWork
+    add_index :guw_guw_coefficient_element_unit_of_works, [:guw_unit_of_work_id, :guw_coefficient_id, :guw_coefficient_element_id], name: :guw_unit_of_work_guw_coefficient_elements unless index_exists?(:guw_guw_coefficient_element_unit_of_works, [:guw_unit_of_work_id, :guw_coefficient_id, :guw_coefficient_element_id], name: :guw_unit_of_work_guw_coefficient_elements)
+
+    #Guw::GuwOutputComplexity
+    add_index :guw_guw_output_complexities, [:guw_complexity_id, :guw_output_id], name: :guw_output_complexities unless index_exists?(:guw_guw_output_complexities, [:guw_complexity_id, :guw_output_id], name: :guw_output_complexities)
+
+    #Guw::GuwOutputComplexityInitialization
+    add_index :guw_guw_output_complexity_initializations, [:guw_complexity_id, :guw_output_id], name: :guw_output_complexity_initializations unless index_exists?(:guw_guw_output_complexity_initializations, [ :guw_complexity_id, :guw_output_id], name: :guw_output_complexity_initializations)
+
+    #Guw::GuwScaleModuleAttribute
+    add_index :guw_guw_scale_module_attributes, [:guw_model_id, :type_attribute], name: :guw_scale_module_attributes unless index_exists?(:guw_guw_scale_module_attributes, [:guw_model_id, :type_attribute], name: :guw_scale_module_attributes)
+
+    #Guw::GuwAttributeComplexity
     add_index :guw_guw_attribute_complexities, [:guw_type_id, :guw_attribute_id], name: :guw_attribute_complexities unless index_exists?(:guw_guw_attribute_complexities, [:guw_type_id, :guw_attribute_id], name: :guw_attribute_complexities)
-    #
-    # #Guw::GuwCoefficientElement
-    # add_index :guw_guw_coefficient_elements, [:guw_model_id, :guw_coefficient_id, :default], name: :guw_coefficient_elements unless index_exists?(:guw_guw_coefficient_elements, [:guw_model_id, :guw_coefficient_id, :default], name: :guw_coefficient_elements)
-    #
-    # #Guw::GuwComplexityCoefficientElement
-    # add_index :guw_guw_complexity_coefficient_elements, [:guw_complexity_id, :guw_coefficient_element_id, :guw_output_id], name: :guw_complexity_coefficient_elements unless index_exists?(:guw_guw_complexity_coefficient_elements, [:guw_complexity_id, :guw_coefficient_element_id, :guw_output_id], name: :guw_complexity_coefficient_elements)
+
+    #Guw::GuwCoefficientElement
+    add_index :guw_guw_coefficient_elements, [:guw_model_id, :guw_coefficient_id, :default], name: :guw_coefficient_elements unless index_exists?(:guw_guw_coefficient_elements, [:guw_model_id, :guw_coefficient_id, :default], name: :guw_coefficient_elements)
+
+    #Guw::GuwComplexityCoefficientElement
+    add_index :guw_guw_complexity_coefficient_elements, [:guw_complexity_id, :guw_coefficient_element_id, :guw_output_id], name: :guw_complexity_coefficient_elements unless index_exists?(:guw_guw_complexity_coefficient_elements, [:guw_complexity_id, :guw_coefficient_element_id, :guw_output_id], name: :guw_complexity_coefficient_elements)
 
 
 
@@ -64,48 +64,48 @@ class AddIndexesToGuwModels < ActiveRecord::Migration
 
 
   def down
-    # #Guw::GuwUnitOfWorkGroup
-    # remove_index :guw_guw_unit_of_work_groups, name: :module_project_guw_groups
-    #
-    # #Guw::GuwType
-    # remove_index :guw_guw_types, name: :guw_model_guw_types
-    # remove_index :guw_guw_types,  name: :guw_model_default_guw_types
-    #
-    # #Guw::GuwUnitOfWork
+    #Guw::GuwUnitOfWorkGroup
+    remove_index :guw_guw_unit_of_work_groups, name: :module_project_guw_groups
+
+    #Guw::GuwType
+    remove_index :guw_guw_types, name: :guw_model_guw_types
+    remove_index :guw_guw_types,  name: :guw_model_default_guw_types
+
+    #Guw::GuwUnitOfWork
     remove_index :guw_guw_unit_of_works, name: :module_project_guw_unit_of_works
-    #
-    # #Guw::GuwComplexity
-    # remove_index :guw_guw_complexities, name: :guw_type_complexities
-    #
-    # #Guw::GuwCoefficient
-    # remove_index :guw_guw_coefficients, name: :guw_model_guw_coefficients
-    #
-    # #Guw::GuwOutput
-    # remove_index :guw_guw_outputs, name: :guw_model_guw_outputs
-    #
-    # #Guw::GuwUnitOfWorkAttribute index important car plusieurs requetes
-    # remove_index :guw_guw_unit_of_work_attributes, name: :guw_unit_of_work_attributes
-    #
-    # #Guw::GuwCoefficientElementUnitOfWork
-    # remove_index :guw_guw_coefficient_element_unit_of_works, name: :guw_unit_of_work_guw_coefficient_elements
-    #
-    # #Guw::GuwOutputComplexity
-    # remove_index :guw_guw_output_complexities, name: :guw_output_complexities
-    #
-    # #Guw::GuwOutputComplexityInitialization
-    # remove_index :guw_guw_output_complexity_initializations, name: :guw_output_complexity_initializations
-    #
-    # #Guw::GuwScaleModuleAttribute
-    # remove_index :guw_guw_scale_module_attributes, name: :guw_scale_module_attributes
-    #
-    # #Guw::GuwAttributeComplexity
+
+    #Guw::GuwComplexity
+    remove_index :guw_guw_complexities, name: :guw_type_complexities
+
+    #Guw::GuwCoefficient
+    remove_index :guw_guw_coefficients, name: :guw_model_guw_coefficients
+
+    #Guw::GuwOutput
+    remove_index :guw_guw_outputs, name: :guw_model_guw_outputs
+
+    #Guw::GuwUnitOfWorkAttribute index important car plusieurs requetes
+    remove_index :guw_guw_unit_of_work_attributes, name: :guw_unit_of_work_attributes
+
+    #Guw::GuwCoefficientElementUnitOfWork
+    remove_index :guw_guw_coefficient_element_unit_of_works, name: :guw_unit_of_work_guw_coefficient_elements
+
+    #Guw::GuwOutputComplexity
+    remove_index :guw_guw_output_complexities, name: :guw_output_complexities
+
+    #Guw::GuwOutputComplexityInitialization
+    remove_index :guw_guw_output_complexity_initializations, name: :guw_output_complexity_initializations
+
+    #Guw::GuwScaleModuleAttribute
+    remove_index :guw_guw_scale_module_attributes, name: :guw_scale_module_attributes
+
+    #Guw::GuwAttributeComplexity
     remove_index :guw_guw_attribute_complexities, name: :guw_attribute_complexities
-    #
-    # #Guw::GuwCoefficientElement
-    # remove_index :guw_guw_coefficient_elements, name: :guw_coefficient_elements
-    #
-    # #Guw::GuwComplexityCoefficientElement
-    # remove_index :guw_guw_complexity_coefficient_elements, name: :guw_complexity_coefficient_elements
+
+    #Guw::GuwCoefficientElement
+    remove_index :guw_guw_coefficient_elements, name: :guw_coefficient_elements
+
+    #Guw::GuwComplexityCoefficientElement
+    remove_index :guw_guw_complexity_coefficient_elements, name: :guw_complexity_coefficient_elements
 
   end
 end
