13ac075059c4d64ba51baa3ac6c134396d83edfd
gsaly <gueyesaly@yahoo.fr>
2017-11-09 15:27:06 +0100

    Correction  WBS  Flagged

--- app/controllers/wbs_activities_controller.rb
+++ app/controllers/wbs_activities_controller.rb
@@ -947,7 +947,6 @@ class WbsActivitiesController < ApplicationController
       end
     end
 
-
     # if Initialize calculation, update the flagged attribute to false
     if initialize_calculation == true
       @module_project_ratio_elements.update_all(flagged: false)
@@ -955,7 +954,7 @@ class WbsActivitiesController < ApplicationController
       # Update flagged Effort or Cost values
       @module_project_ratio_elements.each do |mp_ratio_element|
         wbs_activity_ratio_elt = mp_ratio_element.wbs_activity_ratio_element
-        if wbs_activity_ratio_elt.effort_is_modifiable == true || wbs_activity_ratio_elt.cost_is_modifiable == true
+        if (mp_ratio_element.selected = true) && (wbs_activity_ratio_elt.effort_is_modifiable == true || wbs_activity_ratio_elt.cost_is_modifiable == true)
 
           theoretical_effort = mp_ratio_element.send("theoretical_effort_probable")
           retained_effort = mp_ratio_element.send("retained_effort_probable")
