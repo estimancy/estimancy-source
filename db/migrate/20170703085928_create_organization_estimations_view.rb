bb1768f4bcd3ab5d89cefeb7ee784e098e2ac74e
gsaly <gueyesaly@yahoo.fr>
2017-11-24 14:43:09 +0100

    Module de Taille (UO générique) Exécution : Amélioration de la performance refs: #1738 et #1771

--- db/migrate/20170703085928_create_organization_estimations_view.rb
+++ db/migrate/20170703085928_create_organization_estimations_view.rb
@@ -4,7 +4,8 @@ class CreateOrganizationEstimationsView < ActiveRecord::Migration
   def up
 
     execute <<-SQL
-      CREATE VIEW organization_estimations AS
+
+      CREATE OR REPLACE VIEW organization_estimations AS
       SELECT o.id AS current_organization_id, o.name AS organization_name, p.created_at AS project_created_date, p.id AS project_id, p.*
       FROM projects p
       INNER JOIN organizations o ON o.id = p.organization_id
