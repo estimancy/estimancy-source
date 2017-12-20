cf9d4b95c30c6cb372a253af7b52b8203ef3b000
gsaly <gueyesaly@yahoo.fr>
2017-12-20 12:00:52 +0100

    Vignette WBS fusionnée (effort + Coût)  refs: #1790

--- db/migrate/20171211145119_add_provider_and_request_number_to_projects.rb
+++ db/migrate/20171211145119_add_provider_and_request_number_to_projects.rb
@@ -2,10 +2,10 @@ class AddProviderAndRequestNumberToProjects < ActiveRecord::Migration
 
   def change
     # Ajout du champs forunisseur dans une estimation
-    add_column :projects, :provider_id, :integer
+    add_column :projects, :provider_id, :integer unless column_exists?(:projects, :provider_id)
 
     # Ajout du champs forunisseur dans une estimation
-    add_column :projects, :request_number, :string
+    add_column :projects, :request_number, :string unless column_exists?(:projects, :request_number)
   end
 
 end
--- db/migrate/20171212153928_add_quotation_number_to_organizations.rb
+++ db/migrate/20171212153928_add_quotation_number_to_organizations.rb
@@ -1,6 +1,6 @@
 class AddQuotationNumberToOrganizations < ActiveRecord::Migration
   def change
-    add_column :organizations, :automatic_quotation_number, :string, default: 0
-    add_column :projects, :use_automatic_quotation_number, :boolean
+    add_column :organizations, :automatic_quotation_number, :string, default: 0 unless column_exists?(:organizations, :automatic_quotation_number)
+    add_column :projects, :use_automatic_quotation_number, :boolean unless column_exists?(:projects, :use_automatic_quotation_number)
   end
 end
