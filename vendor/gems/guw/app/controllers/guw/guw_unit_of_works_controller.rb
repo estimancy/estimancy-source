0c2db537ea94a1b2add05de3a99c71767823d20a
nicolasrenard <renard760@gmail.com>
2018-01-19 14:51:52 +0100

    Amélioration des performances lors de l'import

--- vendor/gems/guw/app/controllers/guw/guw_unit_of_works_controller.rb
+++ vendor/gems/guw/app/controllers/guw/guw_unit_of_works_controller.rb
@@ -1089,16 +1089,17 @@ class Guw::GuwUnitOfWorksController < ApplicationController
     @organization = @guw_model.organization
     @project = @module_project.project
     @component = current_component
+    @reload_partial = true
 
     if params["modified_guw_line_ids"].blank?
-      @recalculate = true
-      @guw_unit_of_works = Guw::GuwUnitOfWork.where( organization_id: @organization.id,
-                                                     project_id: @project.id,
-                                                     module_project_id: @module_project.id,
-                                                     pbs_project_element_id: @component.id,
-                                                     guw_model_id: @guw_model.id).includes(:guw_type, :guw_complexity).order("name ASC")
+      @reload_partial = false
+      # @guw_unit_of_works = Guw::GuwUnitOfWork.where( organization_id: @organization.id,
+      #                                                project_id: @project.id,
+      #                                                module_project_id: @module_project.id,
+      #                                                pbs_project_element_id: @component.id,
+      #                                                guw_model_id: @guw_model.id).includes(:guw_type, :guw_complexity).order("name ASC")
     else
-      @recalculate = false
+      @reload_partial = true
       modified_guw_line_ids = params["modified_guw_line_ids"].split(",").uniq.compact
       @guw_unit_of_works = Guw::GuwUnitOfWork.where(id: modified_guw_line_ids).includes(:guw_type, :guw_complexity).order("name ASC")
 
