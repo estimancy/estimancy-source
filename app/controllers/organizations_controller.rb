4eb05cf4c42c61c8c0bf91a2f8ef2833daeb928e
nicolasrenard <renard760@gmail.com>
2018-01-26 10:10:25 +0100

    Correction @guw_types nil

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -2071,7 +2071,7 @@ class OrganizationsController < ApplicationController
       worksheet.add_cell(0, index, name)
     end
 
-    if can?(:manage, Group, :organization_id => @organization.id)
+    if can?(:manage, Group)
       @organization.users.where('login_name <> ?', 'owner').each_with_index do |user, index_line|
         line =  [user.first_name, user.last_name, user.initials,user.email, user.login_name, user.auth_method ? user.auth_method.name : "Application" , user.description, user.language, user.locked_at.nil? ? 0 : 1] + user.groups.where(organization_id: @organization.id).map(&:name)
         line.each_with_index do |my_case, index|
