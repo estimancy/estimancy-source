e500911ff1fbaf631dc34e768149150824f85842
nicolasrenard <renard760@gmail.com>
2018-01-29 10:28:46 +0100

    Export User

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -2135,7 +2135,6 @@ class OrganizationsController < ApplicationController
           end
         end
       else
-        user_groups = @organization.groups.where(name: "*USER")
         @organization.users.where('login_name <> ?', 'owner').each_with_index do |user, index_line|
           line =  [user.first_name, user.last_name, user.initials,user.email, user.login_name, user.auth_method ? user.auth_method.name : "Application" , user.description, user.language, user.locked_at.nil? ? 0 : 1] + ["####"]
           line.each_with_index do |my_case, index|
