a55e00cb5262794ca8bfae4b6006978e81b572cf
gsaly <gueyesaly@yahoo.fr>
2018-01-31 13:17:51 +0100

    Utilitaires d'audit de sécurité refs: #1827

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -14,8 +14,8 @@ if Rails.env == "production"
     # one of the wkhtmltopdf-binary family of gems.
     # exe_path: '/usr/local/bin/wkhtmltopdf',
     #   or
-    # exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
-    exe_path: "/usr/local/rvm/gems/ruby-2.3.3/bin/wkhtmltopdf"
+    exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
+    #exe_path: "/usr/local/rvm/gems/ruby-2.3.3/bin/wkhtmltopdf"
 
     # Layout file to be used for all PDFs
     # (but can be overridden in `render :pdf` calls)
