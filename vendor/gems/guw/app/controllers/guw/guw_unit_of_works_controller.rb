f24d36b3fc43a9960680f125141324395b5a989c
nicolasrenard <renard760@gmail.com>
2018-04-09 15:24:47 +0200

    RubyXL version reverted

--- vendor/gems/guw/app/controllers/guw/guw_unit_of_works_controller.rb
+++ vendor/gems/guw/app/controllers/guw/guw_unit_of_works_controller.rb
@@ -76,9 +76,9 @@ class Guw::GuwUnitOfWorksController < ApplicationController
     @position = params[:hidden_position]
 
     if @position.blank?
-      unless @group.guw_unit_of_works.nil?
+      begin
         @guw_unit_of_work.display_order = @group.guw_unit_of_works.order("display_order ASC").last.display_order.to_i + 1
-      else
+      rescue
         @guw_unit_of_work.display_order = 0
       end
     else
