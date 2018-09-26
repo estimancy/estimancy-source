37dffcc95bad50adacfa06090fb50001cc423413
nicolasrenard <renard760@gmail.com>
2018-09-26 17:07:23 +0200

    Raw data extraction

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -155,7 +155,7 @@ class ProjectsController < ApplicationController
                                                               module_project_id: guow.module_project_id).order("updated_at ASC").last
 
             worksheet_cf.add_cell(i, 13 + j, default.nil? ? 100 : default.value.to_f)
-            worksheet_cf.add_cell(i, 13 + j + 1, ceuw.nil? ? '--' : ceuw.percent.to_f)
+            worksheet_cf.add_cell(i, 13 + j + 1, ceuw.nil? ? nil : ceuw.percent.to_f)
             j = j + 2
           end
         end
@@ -165,8 +165,8 @@ class ProjectsController < ApplicationController
           gat = Guw::GuwAttributeType.where(guw_type_id: guow.guw_type.id,
                                             guw_attribute_id: uowa.guw_attribute_id).first
 
-          worksheet_cf.add_cell(i, 15 + j, gat.nil? ? '-' : gat.default_value)
-          worksheet_cf.add_cell(i, 15 + j + 1, uowa.nil? ? '-' : uowa.most_likely)
+          worksheet_cf.add_cell(i, 17 + j, gat.nil? ? '-' : gat.default_value)
+          worksheet_cf.add_cell(i, 17 + j + 1, uowa.nil? ? '-' : uowa.most_likely)
         end
 
         # if j == 0
