8665db155b2c0b76f4bed15b9870793ab98fcad3
gsaly <gueyesaly@yahoo.fr>
2018-10-09 14:54:42 +0200

    Liste des estimations - Sauvegarde de la recherche effectuée sur plusieurs colonnes refs: #1896 Remise de la valeur de selection du filtre à 4 au lieu de 2

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3088,7 +3088,7 @@ public
     end
 
     # Filtre sur les versions des estimations
-    if !params[:filter_version].in?(['2', ''])
+    if !params[:filter_version].in?(['4', ''])
       res = filter_estimation_versions(res, params[:filter_version])
     end
 
@@ -3214,7 +3214,7 @@ public
     end
 
     # filtre sur la version des estimations
-    if !@filter_version.to_s.in?(['2', ''])
+    if !@filter_version.to_s.in?(['4', ''])
       @organization_estimations = filter_estimation_versions(@organization_estimations, @filter_version)
     end
 
