a1ead90227a31053a0cdff8170279466846ea3c1
nicolasrenard <renard760@gmail.com>
2018-10-10 20:20:53 +0200

    Résolution de bugs pour la demo du 10/10/18

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -2018,7 +2018,7 @@ class OrganizationsController < ApplicationController
     if params[:filter_version].present?
       @filter_version = params[:filter_version]
     else
-      @filter_version = '4'
+      @filter_version = '2'
     end
 
     @object_per_page = (current_user.object_per_page || 10)
@@ -2075,7 +2075,7 @@ class OrganizationsController < ApplicationController
     end
 
     # Filtre sur les versions des estimations
-    if !@filter_version.in?(['4', ''])
+    if !@filter_version.in?(['1', '2', '3', '4' ''])
       res = filter_estimation_versions(res, @filter_version)
     end
 
@@ -2143,7 +2143,7 @@ class OrganizationsController < ApplicationController
     if params[:filter_version].present?
       @filter_version = params[:filter_version]
     else
-      @filter_version = '4'
+      @filter_version = '2'
     end
 
     @object_per_page = (current_user.object_per_page || 10)
@@ -2235,7 +2235,7 @@ class OrganizationsController < ApplicationController
     end
 
     # Filtre sur les versions des estimations
-    if !@filter_version.in?(['4', ''])
+    if !@filter_version.in?(['1', '2', '3' '4', ''])
       res = filter_estimation_versions(res, @filter_version)
     end
 
