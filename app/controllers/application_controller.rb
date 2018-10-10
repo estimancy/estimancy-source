672fbbcfc23de17191976785d159af3db9013850
nicolasrenard <renard760@gmail.com>
2018-10-10 22:01:27 +0200

    Résolution de bugs pour la demo du 10/10/18

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -192,7 +192,7 @@ class ApplicationController < ActionController::Base
         # Le code qui suit remplace les lignes du dessus
         case params[:action]
           when "estimations", "sort", "search", "add_filter_on_project_version"
-            @current_ability ||= Ability.new(current_user, @current_organization, @current_organization.organization_estimations)
+            @current_ability ||= Ability.new(current_user, @current_organization, @current_organization.projects)
           when "projects_from"
             estimation_models = Project.includes(:estimation_status, :project_area, :project_category, :platform_category, :acquisition_category).where(organization_id: @current_organization.id, is_model: true)
             @current_ability ||= Ability.new(current_user, @current_organization, estimation_models)
