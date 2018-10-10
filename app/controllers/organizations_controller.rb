beac5a7f26523b07e5c0a6dc5915903177dadfe8
nicolasrenard <renard760@gmail.com>
2018-10-10 21:12:44 +0200

    Résolution de bugs pour la demo du 10/10/18

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -2015,11 +2015,12 @@ class OrganizationsController < ApplicationController
     set_breadcrumbs I18n.t(:organizations) => "/organizationals_params?organization_id=#{@organization.id}", @organization.to_s => ""
     set_page_title I18n.t(:spec_estimations, parameter: @organization.to_s)
 
-    if params[:filter_version].blank?
-      @filter_version = '4'
-    else
-      @filter_version = params[:filter_version]
-    end
+    # if params[:
+    # _version].blank?
+    #   @filter_version = '4'
+    # else
+    #   @filter_version = params[:filter_version]
+    # end
 
     @object_per_page = (current_user.object_per_page || 10)
 
@@ -2065,7 +2066,7 @@ class OrganizationsController < ApplicationController
 
     res = []
     organization_projects.each do |p|
-      if can?(:see_project, p.project, estimation_status_id: p.project.estimation_status_id)
+      if can?(:see_project, p, estimation_status_id: p.estimation_status_id)
         res << p
       end
 
@@ -2076,7 +2077,7 @@ class OrganizationsController < ApplicationController
 
     # Filtre sur les versions des estimations
     # if !@filter_version.in?(['4', ''])
-      res = filter_estimation_versions(res, @filter_version)
+    #   res = filter_estimation_versions(res, @filter_version)
     # end
 
     @projects = res[@min..@max].nil? ? [] : res[@min..@max-1]
@@ -2140,11 +2141,11 @@ class OrganizationsController < ApplicationController
     set_breadcrumbs I18n.t(:organizations) => "/organizationals_params?organization_id=#{@organization.id}", @organization.to_s => ""
     set_page_title I18n.t(:spec_estimations, parameter: @organization.to_s)
 
-    if params[:filter_version].present?
-      @filter_version = params[:filter_version]
-    else
-      @filter_version = '4'
-    end
+    # if params[:filter_version].present?
+    #   @filter_version = params[:filter_version]
+    # else
+    #   @filter_version = '4'
+    # end
 
     @object_per_page = (current_user.object_per_page || 10)
 
@@ -2236,7 +2237,7 @@ class OrganizationsController < ApplicationController
 
     # Filtre sur les versions des estimations
     # if !@filter_version.in?(['4', ''])
-      res = filter_estimation_versions(res, @filter_version)
+    #   res = filter_estimation_versions(res, @filter_version)
     # end
 
     @projects = res[@min..@max].nil? ? [] : res[@min..@max-1]
