a2a38aaf6ee08e031b602200df78afe5675432d1
nicolasrenard <renard760@gmail.com>
2018-10-16 15:40:23 +0200

    Allow to export PDF

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1101,9 +1101,7 @@ class ProjectsController < ApplicationController
       end
 
       # remplir le champs allow_export_pdf
-      # @project.allow_export_pdf = params['allow_export_pdf']
-
-
+      @project.allow_export_pdf = params['allow_export_pdf']
       @project.save
 
       project_root = @project.root_component
