26484532fb14b46d058160d5ad9a137dfae06c18
nicolasrenard <renard760@gmail.com>
2018-11-20 16:33:08 +0100

    Merge branch 'v2.5' into v3.0
    # Conflicts:
    #	db/schema.rb

--- db/migrate/20181011145615_add_ranges_to_models.rb
+++ db/migrate/20181011145615_add_ranges_to_models.rb
@@ -1,6 +1,6 @@
 class AddRangesToModels < ActiveRecord::Migration
   def change
-    add_column :staffing_staffing_models, :min_range, :integer
-    add_column :staffing_staffing_models, :max_range, :integer
+    add_column :staffing_staffing_models, :min_range, :integer, default: 70
+    add_column :staffing_staffing_models, :max_range, :integer, default: 150
   end
 end
