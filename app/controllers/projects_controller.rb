4d023cee5b06054ef7bb4552a827b9c7722e1bea
nicolasrenard <renard760@gmail.com>
2018-12-19 15:50:19 +0100

    Lorsque le filtre « Afficher toutes les versions » est sélectionné, la pagination se grise et n’est plus fonctionnelle

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -167,7 +167,7 @@ class ProjectsController < ApplicationController
         end
 
 
-        guow.guw_unit_of_work_attributes.where(guw_type_id: guow.guw_type.id).includes(:guw_attribute).order('guw_guw_attributes.name asc').each_with_index do |uowa, j|
+        guow.guw_unit_of_work_attributes.where(guw_type_id: guow.guw_type_id).includes(:guw_attribute).order('guw_guw_attributes.name asc').each_with_index do |uowa, j|
           worksheet_cf.add_cell(i, 19 + j, uowa.nil? ? nil : uowa.most_likely)
         end
 
