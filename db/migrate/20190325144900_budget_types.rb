3433067fefe9b91d256594a8df131898dd29ca60
nntucao <ngtu.caong@gmail.com>
2019-03-27 09:46:42 +0100

    Advanced Research

---
+++ db/migrate/20190325144900_budget_types.rb
@@ -0,0 +1,11 @@
+class BudgetTypes < ActiveRecord::Migration
+  def change
+    create_table :budget_types do |t|
+      t.string :name
+      t.integer :organization_id
+      t.integer :application_id
+
+      t.timestamps null: false
+    end
+  end
+end
