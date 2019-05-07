6d6aef1a750019a6b38e89e1b8cf3d33cec2b38c
nntucao <ngtu.caong@gmail.com>
2019-05-07 14:51:32 +0200

    add Allow Full Screen to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190507083525) do
+ActiveRecord::Schema.define(version: 20190507095120) do
 
   create_table "abacus_organizations", force: :cascade do |t|
     t.float    "value",                          limit: 24
@@ -2335,56 +2335,57 @@ ActiveRecord::Schema.define(version: 20190507083525) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255,   default: "",    null: false
-    t.string   "password_hash",          limit: 255
-    t.string   "password_salt",          limit: 255
-    t.string   "login_name",             limit: 255
-    t.string   "first_name",             limit: 255
-    t.string   "last_name",              limit: 255
-    t.string   "initials",               limit: 255
+    t.string   "email",                       limit: 255,   default: "",    null: false
+    t.string   "password_hash",               limit: 255
+    t.string   "password_salt",               limit: 255
+    t.string   "login_name",                  limit: 255
+    t.string   "first_name",                  limit: 255
+    t.string   "last_name",                   limit: 255
+    t.string   "initials",                    limit: 255
     t.datetime "last_login"
     t.datetime "previous_login"
-    t.string   "time_zone",              limit: 255
-    t.string   "auth_token",             limit: 255
-    t.string   "password_reset_token",   limit: 255
+    t.string   "time_zone",                   limit: 255
+    t.string   "auth_token",                  limit: 255
+    t.string   "password_reset_token",        limit: 255
     t.datetime "password_reset_sent_at"
-    t.integer  "language_id",            limit: 4
-    t.integer  "auth_type",              limit: 4
-    t.text     "ten_latest_projects",    limit: 65535
-    t.integer  "organization_id",        limit: 4
+    t.integer  "language_id",                 limit: 4
+    t.integer  "auth_type",                   limit: 4
+    t.text     "ten_latest_projects",         limit: 65535
+    t.integer  "organization_id",             limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "object_per_page",        limit: 4
-    t.string   "encrypted_password",     limit: 255,   default: "",    null: false
-    t.string   "reset_password_token",   limit: 255
+    t.integer  "object_per_page",             limit: 4
+    t.string   "encrypted_password",          limit: 255,   default: "",    null: false
+    t.string   "reset_password_token",        limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,     default: 0,     null: false
+    t.integer  "sign_in_count",               limit: 4,     default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.string   "confirmation_token",     limit: 255
+    t.string   "current_sign_in_ip",          limit: 255
+    t.string   "last_sign_in_ip",             limit: 255
+    t.string   "confirmation_token",          limit: 255
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.integer  "failed_attempts",        limit: 4,     default: 0,     null: false
-    t.string   "unlock_token",           limit: 255
+    t.integer  "failed_attempts",             limit: 4,     default: 0,     null: false
+    t.string   "unlock_token",                limit: 255
     t.datetime "locked_at"
-    t.string   "provider",               limit: 255
-    t.string   "uid",                    limit: 255
-    t.string   "avatar",                 limit: 255
-    t.integer  "number_precision",       limit: 4
-    t.boolean  "super_admin",                          default: false
+    t.string   "provider",                    limit: 255
+    t.string   "uid",                         limit: 255
+    t.string   "avatar",                      limit: 255
+    t.integer  "number_precision",            limit: 4
+    t.boolean  "super_admin",                               default: false
     t.boolean  "password_changed"
-    t.text     "description",            limit: 65535
+    t.text     "description",                 limit: 65535
     t.datetime "subscription_end_date"
-    t.integer  "originator_id",          limit: 4
-    t.integer  "event_organization_id",  limit: 4
-    t.text     "transaction_id",         limit: 65535
-    t.string   "unconfirmed_email",      limit: 255
-    t.text     "ability",                limit: 65535
-    t.text     "recent_projects",        limit: 65535
+    t.integer  "originator_id",               limit: 4
+    t.integer  "event_organization_id",       limit: 4
+    t.text     "transaction_id",              limit: 65535
+    t.string   "unconfirmed_email",           limit: 255
+    t.text     "ability",                     limit: 65535
+    t.text     "recent_projects",             limit: 65535
     t.boolean  "quick_access"
+    t.boolean  "allow_full_screen_dashboard"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
