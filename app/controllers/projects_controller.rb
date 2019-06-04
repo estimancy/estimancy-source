6139980cffe32a7b64d7918418eeef74b5573af2
nicolasrenard <renard760@gmail.com>
2019-06-04 17:34:23 +0200

    Usabilis Integration

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4239,16 +4239,18 @@ public
                                        project_id: @project.id,
                                        transition_date: Time.now)
 
-      if @project.estimation_status.name == "To Check"
+      # if @project.estimation_status.name == "To Check"
         Thread.new do
           ActiveRecord::Base.connection_pool.with_connection do
-            es = EstimationStatus.where(organization_id: @current_organization.id, name: "Controled")
+            es = EstimationStatus.where(organization_id: @current_organization.id, name: "Controled").first
+            p "TATA"
             sleep(30)
-            @project.estimation_status_id = es
+            p "TOTO"
+            @project.estimation_status_id = es.id
             @project.save
           end
         end
-      end
+      # end
 
       flash[:notice] = I18n.t(:notice_comment_status_successfully_updated)
     else
