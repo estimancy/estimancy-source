81da7344de9cfd26abc7e3e626f0b7c465f382fd
nicolasrenard <renard760@gmail.com>
2019-08-20 13:46:48 +0200

    remove views & wet in organization/setting

--- db/migrate/20190123093309_add_allow_demand_to_organizations.rb
+++ db/migrate/20190123093309_add_allow_demand_to_organizations.rb
@@ -1,5 +1,5 @@
 class AddAllowDemandToOrganizations < ActiveRecord::Migration
   def change
-    # add_column :organizations, :allow_demand, :boolean
+    add_column :organizations, :allow_demand, :boolean
   end
 end
--- db/migrate/20190528075439_create_budget_budget_types.rb
+++ db/migrate/20190528075439_create_budget_budget_types.rb
@@ -1,11 +1,11 @@
 class CreateBudgetBudgetTypes < ActiveRecord::Migration
   def change
-    create_table :budget_budget_types do |t|
-      t.integer :organization_id
-      t.integer :budget_id
-      t.integer :budget_type_id
-
-      t.timestamps null: false
-    end
+    # create_table :budget_budget_types do |t|
+    #   t.integer :organization_id
+    #   t.integer :budget_id
+    #   t.integer :budget_type_id
+    #
+    #   t.timestamps null: false
+    # end
   end
 end
