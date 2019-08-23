4d8bab7cddbeba352ad26850e450bc5a961f39be
nicolasrenard <renard760@gmail.com>
2019-08-23 17:04:48 +0200

    Using AJAX on overview loading

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,15 +19,10 @@
 //= require jquery-number
 //= require spectrum.js
 //= require jscolor
-//= require jquery.gridster.js
-//= require jquery.gridster-0.7.0.js
 
 //= require bootstrap-datepicker
 //= require bootstrap-datepicker/core
 //= require bootstrap-datepicker/locales/bootstrap-datepicker.fr.js
 //= require bootstrap-datepicker/locales/bootstrap-datepicker.de.js
 //= require bootstrap-datepicker/locales/bootstrap-datepicker.it.js
-
-//= require selectize.js
-//= require bootstrap-tokenfield.js
 
