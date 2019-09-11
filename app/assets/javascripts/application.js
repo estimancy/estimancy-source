7999a175268c8116bf7dd6f29b75bcff6358f7ca
gsaly <gueyesaly@yahoo.fr>
2019-09-11 11:42:16 +0200

    Correction bug JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,7 @@
 //= require cookies_eu
 //= require Chart.bundle
 //= require chartkick
-// require jquery
+//= require jquery
 //= require jquery_ujs
 //= require pager.js
 //= require select_list_move
@@ -20,7 +20,7 @@
 //= require spectrum.js
 //= require jscolor
 
-//= require jquery-min.js
+// require jquery-min.js
 //= require jquery-ui-min.js
 
 //= require gridstack.js
