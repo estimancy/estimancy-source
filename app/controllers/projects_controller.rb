9cb70977ddf58d7ab2692e4fa3aa9e9b6937dfea
gsaly <gueyesaly@yahoo.fr>
2019-09-20 18:03:09 +0200

    Ajout des colonnes  « Charge ss prod. (jh) » en colonne AI  et « Coût Services (€) » en colonne AJ

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -201,8 +201,8 @@ class ProjectsController < ApplicationController
 
             @guow_guw_types = Hash.new
 
-            #project.guw_unit_of_works.each do |guow|
-            project.guw_unit_of_works.order("display_order ASC").each do |guow|
+            #project.guw_unit_of_works.order("display_order ASC").each do |guow|
+            project.guw_unit_of_works.each do |guow|
 
               worksheet_cf.add_cell(i, 0, project.title)
               worksheet_cf.add_cell(i, 1, project_application.to_s)
