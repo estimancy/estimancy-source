9376e5eba972e726e03b5b3bbd4f50a404d60e1e
gsaly <gueyesaly@yahoo.fr>
2019-09-20 16:06:41 +0200

    Ajout des colonnes  « Charge ss prod. (jh) » en colonne AI  et « Coût Services (€) » en colonne AJ

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -245,7 +245,7 @@ class ProjectsController < ApplicationController
 
               j = 0
               @guw_coefficients.each do |gc|
-                if gc.coefficient_type == "Pourcentage" || (gc.id == guw_charge_ss_prod_coefficient.id)
+                if gc.coefficient_type == "Pourcentage" || gc.id == guw_charge_ss_prod_coefficient.id
 
                   default = @guw_coefficient_elements.select{ |i| (i.default == true && i.guw_coefficient_id == gc.id ) }.first
                   ceuw = project.guw_coefficient_element_unit_of_works.select{|i| i.guw_coefficient_id == gc.id }.select{|i| i.module_project_id == guow.module_project_id }.last
@@ -255,8 +255,8 @@ class ProjectsController < ApplicationController
                   j = j + 2
 
                   # Charge sans prod en colonne AI
-                  if guw_charge_ss_prod_coefficient && (gc.id == guw_charge_ss_prod_coefficient.id)
-                    worksheet_cf.add_cell(i, 20 + @max_guw_model_attributes_size, (ceuw.percent.blank? ? nil : ceuw.percent.to_f))  # « Charge ss prod. (jh) » en colonne AI
+                  if guw_charge_ss_prod_coefficient && gc.id == guw_charge_ss_prod_coefficient.id
+                    worksheet_cf.add_cell(i, 20 + @max_guw_model_attributes_size, (ceuw.percent.nil ? nil : ceuw.percent.to_f))  # « Charge ss prod. (jh) » en colonne AI
                   end
                 end
               end
