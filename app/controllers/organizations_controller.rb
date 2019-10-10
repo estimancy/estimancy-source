58e640b0223d77271466b59c107768c63475bb61
gsaly <gueyesaly@yahoo.fr>
2019-10-10 18:20:48 +0200

    Correction import profiles

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1130,30 +1130,31 @@ class OrganizationsController < ApplicationController
       tab = workbook[0]
 
       tab.each_with_index do |row, index|
-        if index > 0 && !row[0].value.nil?
+        unless row.nil?
+          if index > 0 && !row[0].value.nil?
+            new_profile = OrganizationProfile.where(name: row[0].value, organization_id: @organization.id).first
+            if new_profile
+              tab_warning_messages << " \n\n #{new_profile.name} : #{I18n.t(:warning_already_exist)}"
+            else
+              begin
+                new_profile = OrganizationProfile.new(name: row[0].value, description: row[1].value, cost_per_hour: row[2].value, organization_id: @organization.id)
+                # new_profile = OrganizationProfile.new(name: row[0].value, description: row[1].value, cost_per_hour: row[2].value,
+                #                                       is_real_profile: row[3].value, associated_services: row[4].value,
+                #                                       r_value: row[5].value, tm_value: row[6].value,
+                #                                       formula: row[7].value, used_cost: row[8].value,
+                #                                       organization_id: @organization.id)
 
-          new_profile = OrganizationProfile.where(name: row[0].value, organization_id: @organization.id).first
-          if new_profile
-            tab_warning_messages << " \n\n #{new_profile.name} : #{I18n.t(:warning_already_exist)}"
-          else
-            begin
-              new_profile = OrganizationProfile.new(name: row[0].value, description: row[1].value, cost_per_hour: row[2].value, organization_id: @organization.id)
-              # new_profile = OrganizationProfile.new(name: row[0].value, description: row[1].value, cost_per_hour: row[2].value,
-              #                                       is_real_profile: row[3].value, associated_services: row[4].value,
-              #                                       r_value: row[5].value, tm_value: row[6].value,
-              #                                       formula: row[7].value, used_cost: row[8].value,
-              #                                       organization_id: @organization.id)
 
-
-              unless new_profile.save
+                unless new_profile.save
+                  tab_error << index + 1
+                end
+              rescue
                 tab_error << index + 1
               end
-            rescue
-              tab_error << index + 1
             end
+          elsif row[0].value.nil?
+            tab_error << index + 1
           end
-        elsif row[0].value.nil?
-          tab_error << index + 1
         end
       end
     else
