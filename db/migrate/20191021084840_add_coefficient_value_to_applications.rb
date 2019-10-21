f913f2a9f3bbb350430492a118beb6b1f3dd0537
gsaly <gueyesaly@yahoo.fr>
2019-10-21 17:45:24 +0200

    Applications criticité

--- db/migrate/20191021084840_add_coefficient_value_to_applications.rb
+++ db/migrate/20191021084840_add_coefficient_value_to_applications.rb
@@ -2,9 +2,5 @@ class AddCoefficientValueToApplications < ActiveRecord::Migration
   def change
     add_column :applications, :criticality, :string
     add_column :applications, :coefficient_label, :string
-
-    # remove_column :applications, :coefficient	, :float
-    # add_column :applications, :coefficient_name	, :string
-    # add_column :applications, :coefficient_value, :float
   end
 end
