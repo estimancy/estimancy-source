bddf026e3df5b97d1e44233e526db14416e8cb96
nicolasrenard <renard760@gmail.com>
2019-10-28 10:51:26 +0100

    Application in guw_tr

--- vendor/gems/guw/app/controllers/guw/guw_unit_of_works_controller.rb
+++ vendor/gems/guw/app/controllers/guw/guw_unit_of_works_controller.rb
@@ -488,11 +488,19 @@ class Guw::GuwUnitOfWorksController < ApplicationController
   def duplicate
     @old_guw_unit_of_work = Guw::GuwUnitOfWork.find(params[:guw_unit_of_work_id])
 
-    # La copie des #guw_unit_of_work_attributes sera geree dans le amoeba_dup
-    @guw_unit_of_work = @old_guw_unit_of_work.amoeba_dup
-    @guw_unit_of_work.save
+    @guw_type = @old_guw_unit_of_work.guw_type
+    uo_guw_type_size = current_module_project.guw_unit_of_works.where(guw_type_id: @guw_type.id).size
 
-    @guw_model = @guw_unit_of_work.guw_model
+    if uo_guw_type_size < @old_guw_unit_of_work.guw_type.maximum
+
+      # La copie des #guw_unit_of_work_attributes sera geree dans le amoeba_dup
+      @guw_unit_of_work = @old_guw_unit_of_work.amoeba_dup
+      @guw_unit_of_work.save
+    else
+      render js: "alert('Vous ne pouvez pas créer plus de #{@guw_type.maximum.to_s} composants pour #{@guw_type.to_s}.')";
+    end
+
+    @guw_model = @old_guw_unit_of_work.guw_model
     @guw_outputs = @guw_model.guw_outputs.where(organization_id: @guw_model.organization_id, guw_model_id: @guw_model.id)
   end
 
