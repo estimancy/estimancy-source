e32f18e6e81c278c6b40082f540f28af58c88436
nicolasrenard <renard760@gmail.com>
2019-11-12 09:55:46 +0100

    Correction Coeff Dynamique

--- vendor/gems/guw/app/controllers/guw/guw_unit_of_works_controller.rb
+++ vendor/gems/guw/app/controllers/guw/guw_unit_of_works_controller.rb
@@ -490,19 +490,19 @@ class Guw::GuwUnitOfWorksController < ApplicationController
 
     @guw_type = @old_guw_unit_of_work.guw_type
 
-    uo_guw_type_size = current_module_project.guw_unit_of_works.where(guw_type_id: @guw_type.id).size
+    # uo_guw_type_size = current_module_project.guw_unit_of_works.where(guw_type_id: @guw_type.id).size
 
-    if uo_guw_type_size.nil?
+    # if uo_guw_type_size.nil?
       @guw_unit_of_work = @old_guw_unit_of_work.amoeba_dup
       @guw_unit_of_work.save
-    else
-      if uo_guw_type_size < @old_guw_unit_of_work.guw_type.maximum
-        @guw_unit_of_work = @old_guw_unit_of_work.amoeba_dup
-        @guw_unit_of_work.save
-      else
-        render js: "alert('Vous ne pouvez pas créer plus de #{@guw_type.maximum.to_s} composants pour #{@guw_type.to_s}.')";
-      end
-    end
+    # else
+    #   if uo_guw_type_size < @old_guw_unit_of_work.guw_type.maximum
+    #     @guw_unit_of_work = @old_guw_unit_of_work.amoeba_dup
+    #     @guw_unit_of_work.save
+    #   else
+    #     render js: "alert('Vous ne pouvez pas créer plus de #{@guw_type.maximum.to_s} composants pour #{@guw_type.to_s}.')";
+    #   end
+    # end
 
     @guw_model = @old_guw_unit_of_work.guw_model
     @guw_outputs = @guw_model.guw_outputs.where(organization_id: @guw_model.organization_id,
@@ -2900,11 +2900,17 @@ class Guw::GuwUnitOfWorksController < ApplicationController
                                                   intermediate_weight: row[18].nil? ? nil : row[18].value,
                                                   guw_type_id: @guw_type.id)
 
-                if module_project.guw_unit_of_works.where(guw_type_id: @guw_type.id).size >= @guw_type.maximum.to_i && !@guw_type.maximum.nil?
-                  guw_uow.selected = false
-                else
+                uo_type = module_project.guw_unit_of_works.where(guw_type_id: @guw_type.id)
+                if uo_type.nil?
                   guw_uow.selected = true
+                else
+                  if module_project.guw_unit_of_works.where(guw_type_id: @guw_type.id).size >= @guw_type.maximum.to_i && !@guw_type.maximum.nil?
+                    guw_uow.selected = false
+                  else
+                    guw_uow.selected = true
+                  end
                 end
+
 
                 guw_uow.save(validate: false)
 
