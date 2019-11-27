84cd826760dcceecfc5b1c481f76668094723371
nicolasrenard <renard760@gmail.com>
2019-11-27 11:48:24 +0100

    Correction GUW TYPE

--- vendor/gems/guw/app/controllers/guw/guw_complexity_work_units_controller.rb
+++ vendor/gems/guw/app/controllers/guw/guw_complexity_work_units_controller.rb
@@ -323,11 +323,7 @@ class Guw::GuwComplexityWorkUnitsController < ApplicationController
     if @guw_type.nil?
       redirect_to :back
     else
-      if @guw_model.default_display == "list"
-        redirect_to guw.guw_type_path(@guw_type)
-      else
-        redirect_to guw.guw_model_path(@guw_model, anchor: "tabs-#{@guw_type.name.gsub(" ", "-")}")
-      end
+      redirect_to guw.guw_type_path(@guw_type)
     end
   end
 end
