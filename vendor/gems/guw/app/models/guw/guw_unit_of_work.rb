7dc77bcbe1b0c62dcc3b2a757cd3a48693c541cb
nicolasrenard <renard760@gmail.com>
2020-03-17 16:38:15 +0100

    Left Panel Refresh

--- vendor/gems/guw/app/models/guw/guw_unit_of_work.rb
+++ vendor/gems/guw/app/models/guw/guw_unit_of_work.rb
@@ -270,8 +270,6 @@ module Guw
         end
       end
 
-      current_module_project.toggle_done
-
       # Reset all view_widget results
       ViewsWidget.reset_nexts_mp_estimation_values(@module_project, component)
     end
