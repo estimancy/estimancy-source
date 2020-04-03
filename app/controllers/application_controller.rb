1f70c9eed0046600e1bde071d0bd282b09e33056
gsaly <gueyesaly@yahoo.fr>
2020-04-03 15:31:11 +0200

    Bugs 4.1 liste estimations refs: #2060

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -204,8 +204,8 @@ class ApplicationController < ActionController::Base
           when "estimations", "sort", "search"
             #@current_ability ||= Ability.new(current_user, @current_organization, @current_organization.projects)
 
-            #@current_ability ||= Abilities.ability_for(current_user, @current_organization, params[:historized])
-            @current_ability ||= AbilityProject.new(current_user, @current_organization, @current_organization.projects)
+            @current_ability ||= Abilities.ability_for(current_user, @current_organization, params[:historized])
+            #@current_ability ||= AbilityProject.new(current_user, @current_organization, @current_organization.projects)
 
           when "projects_from"
             estimation_models = Project.includes(:estimation_status, :project_area, :project_category, :platform_category, :acquisition_category).where(organization_id: @current_organization.id, is_model: true)
