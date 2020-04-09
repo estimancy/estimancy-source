4ab254bd65aa5c5f474538d752694f82e5d2fc19
gsaly <gueyesaly@yahoo.fr>
2020-04-09 14:59:59 +0200

    Correction copie WBS

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3371,7 +3371,8 @@ public
         end
 
         #Managing the component tree : PBS
-        pe_wbs_product = new_prj.pe_wbs_projects.products_wbs.first
+        #pe_wbs_product = new_prj.pe_wbs_projects.products_wbs.first
+        pe_wbs_product = new_prj.pe_wbs_projects.first
 
         # For PBS
         new_prj_components = pe_wbs_product.pbs_project_elements
@@ -3426,8 +3427,8 @@ public
             new_mp.associated_module_projects << new_associated_mp
           end
 
-          Thread.new do
-            ActiveRecord::Base.connection_pool.with_connection do
+          #Thread.new do
+            #ActiveRecord::Base.connection_pool.with_connection do
 
               ### Wbs activity
               #create module_project ratio elements
@@ -3460,9 +3461,9 @@ public
                 #end
                 mp_ratio_element.save
               end
-            end
+            #end
             # ActiveRecord::Base.connection.close
-          end
+          #end
 
 
           ### End wbs_activity
