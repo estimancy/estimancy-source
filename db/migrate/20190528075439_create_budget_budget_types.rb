c37c9e83cb64c80c0e9dbc7c0a95148d0010763d
gsaly <gueyesaly@yahoo.fr>
2020-04-20 09:28:28 +0200

    Correction de budget_budget_types application_budgets application_budget_types

--- db/migrate/20190528075439_create_budget_budget_types.rb
+++ db/migrate/20190528075439_create_budget_budget_types.rb
@@ -1,5 +1,5 @@
 class CreateBudgetBudgetTypes < ActiveRecord::Migration
-  def change
+  def up
     create_table :budget_budget_types do |t|
       t.integer :organization_id
       t.integer :budget_id
@@ -7,5 +7,9 @@ class CreateBudgetBudgetTypes < ActiveRecord::Migration
 
       t.timestamps null: false
     end
+  end
+
+  def down
+    #drop_table :budget_budget_types
   end
 end
