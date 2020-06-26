8414433d98f77d76b5b461b8d2aba1ee7916e8b7
nicolasrenard <renard760@gmail.com>
2020-06-26 10:20:30 +0200

    selected

--- db/migrate/20200113143055_change_decimal_to_value_in_guw_complexity_coefficient_elements.rb
+++ db/migrate/20200113143055_change_decimal_to_value_in_guw_complexity_coefficient_elements.rb
@@ -1,5 +1,6 @@
 class ChangeDecimalToValueInGuwComplexityCoefficientElements < ActiveRecord::Migration
   def up
+    # change_column :guw_guw_complexity_coefficient_elements, :value, :decimal, :precision => 20, :scale => 5
     change_column :guw_guw_complexity_coefficient_elements, :value, :decimal, :precision => 20, :scale => 3
   end
 
