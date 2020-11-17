8219009b941c76f559ee34a5f90fa9806631e145
Kanshi <git@kanshi.fr>
2020-11-17 10:50:08 +0100

    [Docker] Adding 'bundle exec rake assets:precompile' inside the docker image

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 
-# configuring sensitive_settigns.yml
+# configuring sensitive_settings.yml
 sed -i 's/${SECRET_TOKEN}/SECRET_TOKEN_TO_REPLACE/g' config/sensitive_settings.yml
 sed -i 's/${SMTP_ADDRESS}/SMTP_ADDRESS_TO_REPLACE/g' config/sensitive_settings.yml
 if [ -z "${SMTP_PORT}" ]; then SMTP_PORT="587"; fi
@@ -12,9 +12,6 @@ sed -i 's/${SMTP_USER_NAME}/SMTP_USER_NAME_TO_REPLACE/g' config/sensitive_settin
 sed -i 's/${SMTP_PASSWORD}/SMTP_PASSWORD_TO_REPLACE/g' config/sensitive_settings.yml
 sed -i 's/${HOST_URL}/HOST_URL_TO_REPLACE/g' config/sensitive_settings.yml
 sed -i 's/${SMTP_FROM}/SMTP_FROM_TO_REPLACE/g' config/sensitive_settings.yml
-
-# precompiling assets
-bundle exec rake assets:precompile
 
 # If database exists, migrate. Otherweise setup (create and migrate)
 bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:create db:migrate
