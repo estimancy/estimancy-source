9f118123a66f182be9f2e4255519237acbae3926
Nicolas Renard <renard760@gmail.com>
2021-03-15 12:30:51 +0100

    Update entrypoint.sh / Comment line 17

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -14,7 +14,7 @@ sed -i 's/${HOST_URL}/HOST_URL_TO_REPLACE/g' config/sensitive_settings.yml
 sed -i 's/${SMTP_FROM}/SMTP_FROM_TO_REPLACE/g' config/sensitive_settings.yml
 
 # If database exists, migrate. Otherweise setup (create and migrate)
-bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:create db:migrate
+# bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:create db:migrate
 
 # launching server
 rails server --daemon --binding=0.0.0.0 $@
