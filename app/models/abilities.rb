f167240db82cc3a03253c34d3871629dfb211288
nicolasrenard <renard760@gmail.com>
2021-04-22 14:45:21 +0200

    Language EDF

--- app/models/abilities.rb
+++ app/models/abilities.rb
@@ -1,10 +1,10 @@
 module Abilities
 
   def self.ability_for(user, organization, historized)
-    # if historized == "1"
+    if historized == "1"
       AbilityProject.new(user, organization, organization.projects)
-    # else
-    #   AbilityView.new(user, organization, organization.organization_estimations)
-    # end
+    else
+      AbilityView.new(user, organization, organization.organization_estimations)
+    end
   end
 end
